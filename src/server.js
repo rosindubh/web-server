0c29da0435177ab0bef0204b74663df40ede65d7
rosindubh <p.welsby2@ntlworld.com>
2021-09-20 15:21:32 +0100

    added port variable to look for environment port

--- src/server.js
+++ src/server.js
@@ -5,8 +5,9 @@ app.use(express.json());
 
 app.use(helloRouter)
 
-app.listen(5000, () => {
-    console.log("Listening on port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
 });
 
 
